f9673a21ab711ea34e9d34a0a6108317ebe1615b
zchmielewska@gmail.com <zchmielewska@gmail.com>
2021-11-20 12:49:51 +0100

    Use django-heroku

--- optyjaciel/settings.py
+++ optyjaciel/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/zaloguj/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
