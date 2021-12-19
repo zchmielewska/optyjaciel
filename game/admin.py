46236516c8f173e0e95ed7ba3ec7e86d1ba0b32c
zchmielewska@gmail.com <zchmielewska@gmail.com>
2021-12-19 12:45:52 +0100

    register models in admin

--- game/admin.py
+++ game/admin.py
@@ -2,7 +2,10 @@ import game.models
 from django.contrib import admin
 
 
-admin.site.register(game.models.Quiz)
 admin.site.register(game.models.Question)
+admin.site.register(game.models.Quiz)
 admin.site.register(game.models.QuizQuestion)
+admin.site.register(game.models.Answer)
+admin.site.register(game.models.Match)
 admin.site.register(game.models.Suggestion)
+admin.site.register(game.models.Message)
