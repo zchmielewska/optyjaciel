f4a9cb0f658ff086fb61710f5e994bcba2c65150
zchmielewska <zchmielewska@gmail.com>
2022-06-06 19:45:06 +0200

    set default from email

--- optyjaciel/settings.py
+++ optyjaciel/settings.py
@@ -167,7 +167,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_SSL = (os.getenv("EMAIL_USE_SSL") == "True")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
 # Redirect to https
