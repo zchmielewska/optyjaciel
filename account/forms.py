0ab11814e5976cb417cec96db32417c32fbf788e
zchmielewska <zchmielewska@gmail.com>
2022-06-06 17:49:48 +0200

    add smtp

--- account/forms.py
+++ account/forms.py
@@ -1,19 +1,27 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Powtórz hasło", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email")
+        fields = ("username", "email")
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        try:
+            password_validation.validate_password(password, self.instance)
+        except forms.ValidationError as error:
+            self.add_error("password", error)
+        return password
 
     def clean_password2(self):
         cd = self.cleaned_data
         if cd["password"] != cd["password2"]:
             raise forms.ValidationError("Podane hasła różnią się.")
         return cd["password2"]
-
 
--- optyjaciel/settings.py
+++ optyjaciel/settings.py
@@ -159,5 +159,16 @@ LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/account/login"
 LOGOUT_URL = "/account/logout"
 
+
+# E-mail backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_SSL = (os.getenv("EMAIL_USE_SSL") == "True")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+
+
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
