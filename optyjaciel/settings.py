d026029d9383f461868a84546c91d6b84444cdec
zchmielewska <zchmielewska@gmail.com>
2023-03-25 15:25:02 +0100

    update settings for gmail

--- optyjaciel/settings.py
+++ optyjaciel/settings.py
@@ -170,7 +170,7 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_SSL = (os.getenv("EMAIL_USE_SSL") == "True")
+EMAIL_USE_TLS = (os.getenv("EMAIL_USE_TLS") == "True")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
