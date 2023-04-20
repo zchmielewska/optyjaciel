ce726af981e7c61039172b968da4859f03f05683
zchmielewska <zchmielewska@gmail.com>
2023-04-20 08:57:47 +0200

    undo unique email constraint

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
 # Changes to the built-in User model
-User._meta.get_field('email')._unique = True
-User._meta.get_field('email').blank = False
-User._meta.get_field('email').null = False
+# User._meta.get_field('email')._unique = True
+# User._meta.get_field('email').blank = False
+# User._meta.get_field('email').null = False
