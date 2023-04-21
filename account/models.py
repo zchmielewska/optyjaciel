b7bac2498eb34a473978d54552f253a91746568f
zchmielewska <zchmielewska@gmail.com>
2023-04-21 19:40:40 +0200

    remove get_username()

--- account/models.py
+++ account/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
-
-# Changes to the built-in User model
-# User._meta.get_field('email').blank = False
-# User._meta.get_field('email').null = False
 
 
 class Profile(models.Model):
