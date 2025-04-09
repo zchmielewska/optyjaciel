5e3145b35b19336697203216ad64680ee047bceb
zchmielewska <zchmielewska@gmail.com>
2025-04-09 19:38:39 +0200

    add blog

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from game import models
 
-# Register your models here.
+
+@admin.register(models.Post)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
